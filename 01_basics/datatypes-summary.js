fcef85fef4fec5d171e62668e7054adbbe0fbfe9
FarrukhGul99 <farrukhgul2002@icloud.com>
2024-06-22 23:38:46 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,3 +2,30 @@
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
+
+// const score = 100;
+
+// const id = symbol('123');
+// const anotherId = symbol('123');
+
+// console.log(id === anotherId);
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["spiderman", "superman", "uchiha"];
+
+let myObj = {
+    name: "farrukh",
+    age: 22,
+}
+
+let bigNumber = 786876786767876768789n;
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof(bigNumber));
