adf3cf15b981d6f7699a67229f21dc9bf36a3063
FarrukhGul99 <farrukhgul2002@icloud.com>
2024-06-23 15:38:29 +0000

    array basics

--- 01_basics/06_stack and heap memory.js
+++ 01_basics/06_stack and heap memory.js
@@ -1,4 +1,21 @@
 //stack memory  (Primitive),  Heap memory (Non-Premitive)
 
-let mnYoutubeName = "farrukhgul.com";
+let myYoutubeName = "farrukhgul.com";
 
+let anotherName = myYoutubeName;
+anotherName = "chaiorcode";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    emial: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "farrukh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,54 @@
+// Arrays
+
+let myArr = [10, 20, 30, 40, 50 ,60];
+
+let myHeroes = ["spiderman", "superman", "edward"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+// console.log(myArr[1]);
+
+// Arrays Methods
+
+// myArr.push(70);   // add element at the end of array
+// myArr.push(80);
+// myArr.pop();      // remove element at the end of array
+
+// myArr.unshift(5)     // add element at the begining of array
+// myArr.shift();       // remove element at the begining of array
+
+
+// console.log(myArr.includes(5));  // checking wheather particular element is include or not
+
+// console.log(myArr.indexOf(9));
+
+
+// const newArr = myArr.join();    
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice , splice
+
+// console.log("A ", myArr);
+
+// const newArr = myArr.slice(1, 3)
+// console.log(newArr);
+
+// console.log("B ", myArr);
+
+
+
+console.log("A ", myArr);
+
+const newArr2 = myArr.splice(1, 3)
+console.log(newArr2);
+
+console.log("B ", myArr);
+
+
+
+
+
+
+
