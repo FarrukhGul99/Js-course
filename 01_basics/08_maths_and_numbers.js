006bcbc0b0b2762e3b00b749fffc0a970910a5d4
FarrukhGul99 <farrukhgul2002@icloud.com>
2024-06-23 00:53:55 +0000

    maths and numbers

---
+++ 01_basics/08_maths_and_numbers.js
@@ -0,0 +1,39 @@
+// *************** NUMBERS ******************
+// const balance = 899.899898;
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// **************** MATHS *****************
+console.log(Math);
+// console.log(Math.abs(-4)); // abs conver negative value into positive
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));  //top value 5
+// console.log(Math.floor(4.8)); // lower value like 4 
+
+// console.log(Math.min(2,4,6,8,10));
+// console.log(Math.max(2,4,6,8,10));
+
+// console.log((Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
